d297a1b1df24f7c067148176449b375456388916
Tua Hoang Nguyen <hoangtua.vn@gmail.com>
2019-09-05 09:59:14 +0700

    use LogCapture to record the stream

--- decorator.py
+++ decorator.py
@@ -1,8 +1,13 @@
 
+from testfixtures import LogCapture
+
 def log2mongo(func):
     def wrapper(*args, **kwargs):
-        returned_value = func(*args, **kwargs)
+        with LogCapture() as root_log:
+            returned_value = func(*args, **kwargs)
         # log something here to MongoDB
-        print(f'>>> pushing {returned_value} to MongoDB')
+        print(f'>>> Start pushing to MongoDB')
+        print(root_log.records)
+        print(f'>>> End pushing to MongoDB')
         return returned_value
     return wrapper
