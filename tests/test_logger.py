7b97044617b15cecdec4f4b32de5ad04bb114079
Tua Hoang Nguyen <hoangtua.vn@gmail.com>
2019-09-05 09:27:41 +0700

    add unit test

---
+++ tests/test_logger.py
@@ -0,0 +1,38 @@
+import unittest
+import logging
+from testfixtures import LogCapture
+
+from logger import get_logger
+
+
+class TestLogger(unittest.TestCase):
+    def test_get_logger(self):
+        logger = get_logger('LOG NAME', logging.DEBUG)
+        INP = [
+            "some debug",
+            "some info",
+            "MY MESSAGE",
+            "what error?",
+            "the critical one",
+        ]
+        EXP = [
+            "LOG NAME", "DEBUG", "some debug",
+            "INFO", "some info",
+            "WARNING", "MY MESSAGE",
+            "ERROR", "what error?",
+            "CRITICAL", "the critical one",
+        ]
+        with LogCapture() as log:
+            logger.debug(INP[0])
+            logger.info(INP[1])
+            logger.warning(INP[2])
+            logger.error(INP[3])
+            logger.critical(INP[4])
+
+        assert EXP[0] in str(log)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
